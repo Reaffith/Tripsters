3952edc1d428f817f6a3d1e905de44cb849fa56d
Taras Nechyporuck <tarasnechyporuck@gmail.com>
2024-10-25 20:26:44 +0300

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/header/Header";
+import { Footer } from "./Footer/Footer";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
       <Outlet />
 
-      <footer></footer>
+      <Footer />
     </>
   );
 }
