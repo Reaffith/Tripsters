2666b9b22ac1395f8d9c1c394088ec3c68c2cd72
Taras Nechyporuck <tarasnechyporuck@gmail.com>
2024-11-13 20:39:23 +0200

    test2

--- src/main/java/com/example/tripsters/controller/FriendShipController.java
+++ src/main/java/com/example/tripsters/controller/FriendShipController.java
@@ -6,10 +6,7 @@ import com.example.tripsters.dto.friendship.UpdateFriendShipStatusDto;
 import com.example.tripsters.service.FriendShipService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
-=======
 import org.springframework.security.access.prepost.PreAuthorize;
->>>>>>> front-deploy
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
