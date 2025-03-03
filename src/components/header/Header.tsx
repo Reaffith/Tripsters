08960938721ef42d6d84aa274a2c0fae0c3b53eb
Taras Nechyporuck <tarasnechyporuck@gmail.com>
2025-03-03 20:48:16 +0200

    add changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Logo from "../../pics/logo.svg";
